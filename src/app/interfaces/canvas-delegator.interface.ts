4ee2967bfaaf8eb67c43f38bf6a53e18123b55ac
Davide Perozzi <triplexp@live.de>
2019-03-25 20:52:51 +0100

    cleanup

--- src/app/interfaces/canvas-delegator.interface.ts
+++
@@ -1,9 +0,0 @@
-import { DefaultContainer } from '../canvas/default.container';
-
-export interface CanvasDelegator {
-  name: string;
-
-  containersUpdated(containers: DefaultContainer[]): void;
-  syncContainers(updateViewport: boolean): void;
-  renderContainers(): void;
-}
